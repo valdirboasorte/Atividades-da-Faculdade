3eadfd2bfa60c3f9ee9512ba81fd73afe978ac32
Valdir Fernandes <110644615+valdirboasorte@users.noreply.github.com>
2025-03-18 20:06:08 -0300

    Letras.java

--- Letras.java
+++ Letras.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 
 public class Letras {
     public static void main(String[] args) {
-    Scanner teclado = new Scanner(System.in);
-    System.out.println("Digite sua frase: ");
-    String frase = teclado.nextLine();
-    int tamanho = frase.length();
-    boolean maior = tamanho > 15;
-    System.out.println("sua frase: " + frase);
-    System.out.println(frase + " tem " + tamanho + " caracteres");
-    System.out.println("Tem mais de 15 letras? " + maior);
+    Scanner teclado = new Scanner(System.in); // scanner que verifica os seus inputs no teclado para formular uma frase
+    System.out.println("Digite sua frase: "); //parte onde vc vai digitar a sua frase
+    String frase = teclado.nextLine(); //sua frase que vai aparecer na tela
+    int tamanho = frase.length(); // verifica o tamanho da frase
+    boolean maior = tamanho > 15; // se o tamanho for maior que 15 letras vai ser false
+    System.out.println("sua frase: " + frase); // mostra a frase que vc digitou anteriormente
+    System.out.println(frase + " tem " + tamanho + " caracteres"); // mostra quantos caracteres forma a sua frase
+    System.out.println("Tem mais de 15 letras? " + maior); // e verifica se sua frase tem mais de 15 letras 
 
     
     
