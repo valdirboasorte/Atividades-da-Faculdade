9405733218344228a8ce91916c00d165f09cbf2e
Valdir Fernandes <110644615+valdirboasorte@users.noreply.github.com>
2025-03-18 19:44:21 -0300

    salariominimo.java

--- salariominimo.java
+++ salariominimo.java
@@ -1,13 +1,13 @@
 package salariominimo;
-import java.util.Scanner;
+import java.util.Scanner; 
 
 public class salariominimo {
     public static void main(String[] args) {
     Scanner teclado = new Scanner(System.in);
     System.out.println("Digite o seu salario: ");
-    int salario = teclado.nextInt();
-    int sm = salario / 1412;
-    System.out.println("a sua quantidade de salarios minimos é de : " + sm);
+    int salario = teclado.nextInt(); //salario do funcionario
+    int sm = salario / 1412; // calculo para transformar seu salario em quantidades de salarios minimos 1412 = 1sm 
+    System.out.println("a sua quantidade de salarios minimos é de : " + sm); // exibe a quantidade de salarios minimos na tela
     
     }
 }
